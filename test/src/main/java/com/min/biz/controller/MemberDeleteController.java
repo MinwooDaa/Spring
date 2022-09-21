2faacb20377eaed01e85e8c1bf3c909f123d279e
MinwooDaa <qte1512@naver.con>
2022-09-21 14:57:53 +0900

    Revert "Controller 어노테이션, HandlerMapping, Command 객체"
    This reverts commit 83ee3027d0c73224496ea1929b69d4d3d8722c92.

--- test/src/main/java/com/min/biz/controller/MemberDeleteController.java
+++
@@ -1,5 +0,0 @@
-package com.min.biz.controller;
-
-public class MemberDeleteController {
-	
-}
--- test1/src/main/java/test/Watchs.java
+++
@@ -1,6 +0,0 @@
-package test;
-
-public interface Watchs {
-	public void volumeUp();
-	public void volumeDown();
-}
